d65383462250f31022be2f2759360463bd7a5156
BAUCHET <julien.bauchet@capgemini.com>
2019-01-24 12:19:54 +0100

    insert BDD

--- src/main/java/com/example/kafka/DemoConsumer.java
+++ src/main/java/com/example/kafka/DemoConsumer.java
@@ -94,7 +94,7 @@ public class DemoConsumer implements InitializingBean{
 					
 					Connection connection = dataSource.getConnection();
 
-					PreparedStatement stmt = connection.prepareStatement("insert into salesforce.ple_idx (siren,sfid, step, state, idIdx) values (?,?,?,?,?)" );
+					PreparedStatement stmt = connection.prepareStatement("insert into salesforce.ple_idx (siren,sfid, step, state, ididx) values (?,?,?,?,?)" );
 					stmt.setString(1, ple.getInsee());
 					stmt.setString(2, ple.getSfid());
 					stmt.setString(3, "pricing");
